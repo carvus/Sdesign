64ec4c6df416057e9c9a971415999ebdffe4aa5d
carvus <armen.demirchyan.ait@gmail.com>
2021-07-14 00:20:06 -0700

    Select current lang

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/header.scss';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const [ currentLang, setCurrentLang ] = useState("arm");
+
     return (
         <div className="header">
             <img className="header-logo" src="images/bigger-logo2.png" alt="" />
@@ -12,8 +14,8 @@ export default function Header() {
                     <img src="images/home/icons/three-lines.png" alt="" />
                 </label>
                 <div className="languages">
-                    <span className="active">ARM</span>
-                    <span>ENG</span>
+                    <span onClick={()=>setCurrentLang(`arm`)} className={`${ currentLang === `arm` ? `active` : `` }`}>ARM</span>
+                    <span onClick={()=>setCurrentLang(`eng`)} className={`${ currentLang !== `arm` ? `active` : `` }`}>ENG</span>
                 </div>
                 <nav>
                     <ul>
