968ac254a219197e87df14d3610e50eb6426904f
carvus <armen.demirchyan.ait@gmail.com>
2021-07-14 02:39:12 -0700

    contact design fixes

--- src/components/Contacts/ContactUsForm.jsx
+++ src/components/Contacts/ContactUsForm.jsx
@@ -5,27 +5,25 @@ export default function ContactUsForm() {
     const [userName, setUserName] = useState("")
     const [userEmail, setUserEmail] = useState("")
     return (
-        <div>
-            <form action="" className="contact-form">
-                <div className="name-mail">
-                    <input type="text"
-                        placeholder="Անուն"
-                        name="username"
-                        id="username"
-                        value={userName}
-                        onInput={(e)=>setUserName(e.target.value)}
-                    />
-                    <input type="text"
-                        placeholder="Էլ փոստ"
-                        name="email" 
-                        id="useremail" 
-                        value={userEmail}
-                        onInput={(e)=>setUserEmail(e.target.value)}
-                    />
-                </div>
-                <textarea name="" id="" rows="10"></textarea>
-                <button className="submit-btn">Ուղղարկել<span> →</span></button>
-            </form>
-        </div>
+        <form action="" className="contact-form">
+            <div className="name-mail">
+                <input type="text"
+                    placeholder="Անուն"
+                    name="username"
+                    id="username"
+                    value={userName}
+                    onInput={(e)=>setUserName(e.target.value)}
+                />
+                <input type="text"
+                    placeholder="Էլ փոստ"
+                    name="email" 
+                    id="useremail" 
+                    value={userEmail}
+                    onInput={(e)=>setUserEmail(e.target.value)}
+                />
+            </div>
+            <textarea name="" id="" rows="10"></textarea>
+            <button className="submit-btn">Ուղղարկել<span> →</span></button>
+        </form>
     )
 }
