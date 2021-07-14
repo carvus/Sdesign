23d1ebac115140974f3eac0548e7348910ccdb97
carvus <armen.demirchyan.ait@gmail.com>
2021-07-14 00:09:14 -0700

    Home design fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,44 +4,42 @@ import '../../css/home/footer.scss';
 export default function Footer() {
     return (
         <div className="footer">
-            <div className="footer-contacts">
-                <div className="contact-info">
-                    <p>ՀՀ,0033 ք, Երևան Գյուլբենկյան 31</p>
-                    <img src="images/home/icons/footer-icons/location.png" alt="" />
-                    <p>ՀՀ,4001 ք, Իջևան Վասիլյան 2/2</p>
+            <div className="footer-content">
+                <div className="footer-contacts">
+
+                    <p className="info-1-column-item">ՀՀ,0033 ք, Երևան Գյուլբենկյան 31</p>
+                    <img className="icon-column-item" src="images/home/icons/footer-icons/location.png" alt="" />
+                    <p className="info-2-column-item">ՀՀ,4001 ք, Իջևան Վասիլյան 2/2</p>
+
+                    <p className="info-1-column-item">011 700 992 <br /> 096 23 43 43 </p>
+                    <img className="icon-column-item" src="images/home/icons/footer-icons/phone.png" alt="" />
+                    <p className="info-2-column-item">091 713 913</p>
+
+                    <p className="info-1-column-item">+37477000992</p>
+                    <img className="icon-column-item" src="images/home/icons/footer-icons/viber.png" alt="" />
+                    <p className="info-2-column-item">+37493050503</p>
+
+                    <p className="info-1-column-item">info.sdesignstudio@gmail.com</p>
+                    <img className="icon-column-item" src="images/home/icons/footer-icons/mail.png" alt="" />
+                    <p className="info-2-column-item">sdesign.ijevan@gmail.com</p> 
                 </div>
-                <div className="contact-info">
-                    <p>011 700 992 <br /> 096 23 43 43 </p>
-                    <img src="images/home/icons/footer-icons/phone.png" alt="" />
-                    <p>091 713 913</p>
-                </div>
-                <div className="contact-info">
-                    <p>+37477000992</p>
-                    <img src="images/home/icons/footer-icons/viber.png" alt="" />
-                    <p>+37493050503</p>
-                </div>
-                <div className="contact-info">
-                    <p>info.sdesignstudio@gmail.com</p>
-                    <img src="images/home/icons/footer-icons/mail.png" alt="" />
-                    <p>sdesign.ijevan@gmail.com</p>
-                </div>
-            </div>
-            <div className="our-works">
-                <h3>Մենք կատարում ենք</h3>
-                <ul>
-                    <li>ԴԻԶԱՅՆ</li>
-                    <li>ՎԱՀԱՆԱԿՆԵՐ</li>
-                    <li>ԼԱՅՆԱՖՈՐՄԱՏ ՏՊԱԳՐՈՒԹՅՈՒՆ</li>
-                    <li>ՑՈՒՑԱՆԱԿՆԵՐ</li>
-                    <li>ԾԱՎԱԼԱՅԻՆ ՏԱՌԵՐ</li>
-                    <li> 3D ՓՈՐԱԳՐՈՒԹՅՈՒՆ</li>
-                    <li>ԻՆՏԵՐԻԵՐԻ և ԷՔՍՏԵՐԻԵՐԻ ԴԻԶԱՅՆ</li>
-                    <li>ԼԱԶԵՐԱՅԻՆ և ՖՐԵԶԵՐԱՅԻՆ ԱՇԽԱՏԱՆՔՆԵՐ</li>
-                </ul>
-                <div>
-                    <img src="images/home/icons/footer-icons/instagram.png" alt="" />
-                    <img src="images/home/icons/footer-icons/facebook.png" alt="" />
-                    <p>© 2021 Sdesign. All Rights Reserved.</p>
+                <div className="our-works">
+                    <h3>Մենք կատարում ենք</h3>
+                    <ul>
+                        <li>ԴԻԶԱՅՆ</li>
+                        <li>ՎԱՀԱՆԱԿՆԵՐ</li>
+                        <li>ԼԱՅՆԱՖՈՐՄԱՏ ՏՊԱԳՐՈՒԹՅՈՒՆ</li>
+                        <li>ՑՈՒՑԱՆԱԿՆԵՐ</li>
+                        <li>ԾԱՎԱԼԱՅԻՆ ՏԱՌԵՐ</li>
+                        <li> 3D ՓՈՐԱԳՐՈՒԹՅՈՒՆ</li>
+                        <li>ԻՆՏԵՐԻԵՐԻ և ԷՔՍՏԵՐԻԵՐԻ ԴԻԶԱՅՆ</li>
+                        <li>ԼԱԶԵՐԱՅԻՆ և ՖՐԵԶԵՐԱՅԻՆ ԱՇԽԱՏԱՆՔՆԵՐ</li>
+                    </ul>
+                    <div>
+                        <img src="images/home/icons/footer-icons/instagram.png" alt="" />
+                        <img src="images/home/icons/footer-icons/facebook.png" alt="" />
+                        <p>© 2021 Sdesign. All Rights Reserved.</p>
+                    </div>
                 </div>
             </div>
         </div>
